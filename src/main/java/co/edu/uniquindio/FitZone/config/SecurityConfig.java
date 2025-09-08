3469b04f71084098e5647ae351492b8337567eee
JuanDavidLopezH <juand.lopezh@uqvirtual.edu.co>
2025-09-07 23:57:43 -0500

    se actualizan los cors

--- src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://frontend-fitzone-oaoh.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://frontend-fitzone-oaoh.vercel.app", "https://frontend-fitzone-oaoh-pk5wh3bel-juan-lopez-s-projects.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setAllowCredentials(true);
