a73c3b394453ca98efe32cd48daef0a60c68b532
JuanDavidLopezH <juand.lopezh@uqvirtual.edu.co>
2025-09-22 22:33:05 -0500

    se actualiza la configuracion de seguridad con el fin de permitir la conexion entre back y front

--- src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
                 "https://frontend-fitzone-oaoh.vercel.app",
-                "https://frontend-fitzone-oaoh-eqi6qc26k-juan-lopez-s-projects.vercel.app"
+                "https://frontend-fitzone-oaoh-eqi6qc26k-juan-lopez-s-projects.vercel.app",
+                "https://frontend-fitzone-oaoh-6u53o5wsw-juan-lopez-s-projects.vercel.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
