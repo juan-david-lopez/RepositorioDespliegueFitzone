4d71c2997f54c3d9057686878a9c5cc696819748
JuanDavidLopezH <juand.lopezh@uqvirtual.edu.co>
2025-10-19 22:48:03 -0500

    cambio cors java

--- src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/FitZone/config/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
                 "http://localhost:3001",
                 "https://frontend-fitzone-oaoh.vercel.app",
                 "https://frontend-fitzone-oaoh-git-feat-mem-ed8561-juan-lopez-s-projects.vercel.app",
-                "https://front-proyecto-psi.vercel.app"  // ✅ NUEVO: Frontend principal en Vercel
+                "https://front-proyecto-46dcg9gxe-juan-lopez-s-projects.vercel.app/",
+                "https://front-proyecto-psi.vercel.app/"// ✅ NUEVO: Frontend principal en Vercel
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
